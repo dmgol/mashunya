0831505377dab7310daa913f966e46fdfe526ca8
Dmitry Golovanov <dmgolovanov@gmail.com>
2017-01-30 17:26:53 +0300

    getProduct change

--- app/server/serve_clients.go
+++ app/server/serve_clients.go
@@ -13,30 +13,35 @@ import (
 
 func getProduct(ctx *gin.Context) {
 	var (
-		product        models.Product
-		colorVariation models.ColorVariation
-		codes          = strings.Split(ctx.Param("code"), "_")
-		productCode    = codes[0]
-		colorCode      string
+		product models.Product
+		//colorVariation models.ColorVariation
+		codes       = strings.Split(ctx.Param("code"), "_")
+		productCode = codes[0]
+		//colorCode      string
 	)
 
-	if len(codes) > 1 {
-		colorCode = codes[1]
+	// if len(codes) > 1 {
+	// 	colorCode = codes[1]
+	// }
+
+	if db.DB.Debug().Preload("ColorVariations").Preload("ColorVariations.Color").Preload("Category").Where(&models.Product{Code: productCode}).First(&product).RecordNotFound() {
+		ctx.JSON(http.StatusNotFound,
+			gin.H{
+				"Product": "Not found",
+			})
+		return
 	}
 
-	if db.DB.Where(&models.Product{Code: productCode}).First(&product).RecordNotFound() {
-		http.Redirect(ctx.Writer, ctx.Request, "/", http.StatusFound)
-	}
-
-	db.DB.Preload("Product").Preload("Color").Preload("SizeVariations.Size").Where(&models.ColorVariation{ProductID: product.ID, ColorCode: colorCode}).First(&colorVariation)
+	//db.DB.Preload("Product").Preload("Color").Preload("SizeVariations.Size").Where(&models.ColorVariation{ProductID: product.ID, ColorCode: colorCode}).First(&colorVariation)
 
 	ctx.JSON(http.StatusOK,
 		gin.H{
-			"Product":        product,
-			"ColorVariation": colorVariation,
+			"Product": product,
+			//"ColorVariation": colorVariation,
 		})
 }
 
+// ServeClients serves client request
 func ServeClients() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
